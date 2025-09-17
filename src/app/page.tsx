03b2325027ab47951d8c32c57439804b79212b2a
Kevin Fries <kevinsctfries@gmail.com>
2025-09-17 14:10:37 -0400

    Update styling and add sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,46 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 
+export const metadata = {
+  title: "Home | Lumora",
+  description: "Welcome to Lumora, your partner in digital excellence.",
+};
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Welcome to Lumora</h1>
-        <p>Experience cutting-edge web solutions.</p>
+        <section className={styles.sectionHero}>
+          <h1>Lumora</h1>
+          {/* Design logo section here */}
+        </section>
+        <section className={styles.sectionContent}>
+          <h2>Section</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            saepe, tempore voluptatibus neque dolores aut quis rem quam corrupti
+            nostrum dolore debitis fugiat vero numquam! Dolores culpa aut
+            voluptatem impedit!
+          </p>
+        </section>
+        <section className={styles.sectionContent}>
+          <h2>Section</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            saepe, tempore voluptatibus neque dolores aut quis rem quam corrupti
+            nostrum dolore debitis fugiat vero numquam! Dolores culpa aut
+            voluptatem impedit!
+          </p>
+        </section>
+        <section className={styles.sectionContent}>
+          <h2>Section</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            saepe, tempore voluptatibus neque dolores aut quis rem quam corrupti
+            nostrum dolore debitis fugiat vero numquam! Dolores culpa aut
+            voluptatem impedit!
+          </p>
+        </section>
       </main>
     </div>
   );
