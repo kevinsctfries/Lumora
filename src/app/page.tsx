ea9f002f060494a3c216d53a420d8c1c75c42389
Kevin Fries <kevinsctfries@gmail.com>
2025-09-18 20:33:47 -0400

    fix: fix flashing of logo animation
    - fix logo flashing bug during animation
    - update styling for sectionContent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,94 +30,88 @@ export default function Home() {
         </section>
 
         <section className={styles.sectionContent}>
-          <h2>Section</h2>
           <div className={styles.contentRow}>
             <div>
+              <h2>Section</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo saepe, tempore voluptatibus neque dolores aut quis rem
-                quam corrupti nostrum dolore debitis fugiat vero numquam!
-                Dolores culpa aut voluptatem impedit!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat, mollitia. Ea aspernatur dolorem ipsum aliquam.
+                Inventore dignissimos fugiat laborum blanditiis sed libero, quae
+                a vel necessitatibus, omnis perferendis voluptate voluptas.
               </p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic,
-                repudiandae? Mollitia atque distinctio excepturi nihil officiis
-                explicabo porro voluptates quam praesentium at illo, aliquam ut
-                impedit eos eum sed? Sequi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                velit rem aliquid reprehenderit, nam quod eaque neque mollitia
+                consequuntur. Ea, pariatur voluptas? Natus omnis, ipsam pariatur
+                voluptatem cupiditate quam ex.
               </p>
             </div>
             <div className={styles.imageContainer}>
-              <div className={styles.imagePlaceholder}>
-                <Image
-                  src="/undraw_mobile-payments_uate.svg"
-                  alt="Mobile payments illustration"
-                  fill
-                  style={{ objectFit: "contain" }}
-                  sizes="(max-width: 1200px) 100vw, 400px"
-                />
-              </div>
+              <Image
+                src="/undraw_mobile-payments_uate.svg"
+                alt="Mobile payments illustration"
+                fill
+                style={{ objectFit: "contain" }}
+                sizes="(max-width: 1200px) 100vw, 400px"
+              />
             </div>
           </div>
         </section>
 
         <section className={styles.sectionContent}>
-          <h2>Section</h2>
           <div className={styles.contentRow}>
             <div>
+              <h2>Section</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo saepe, tempore voluptatibus neque dolores aut quis rem
-                quam corrupti nostrum dolore debitis fugiat vero numquam!
-                Dolores culpa aut voluptatem impedit!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat, mollitia. Ea aspernatur dolorem ipsum aliquam.
+                Inventore dignissimos fugiat laborum blanditiis sed libero, quae
+                a vel necessitatibus, omnis perferendis voluptate voluptas.
               </p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic,
-                repudiandae? Mollitia atque distinctio excepturi nihil officiis
-                explicabo porro voluptates quam praesentium at illo, aliquam ut
-                impedit eos eum sed? Sequi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                velit rem aliquid reprehenderit, nam quod eaque neque mollitia
+                consequuntur. Ea, pariatur voluptas? Natus omnis, ipsam pariatur
+                voluptatem cupiditate quam ex.
               </p>
             </div>
             <div className={styles.imageContainer}>
-              <div className={styles.imagePlaceholder}>
-                <Image
-                  src="/undraw_chat-interface_vofq.svg"
-                  alt="Chat illustration"
-                  fill
-                  style={{ objectFit: "contain" }}
-                  sizes="(max-width: 1200px) 100vw, 400px"
-                />
-              </div>
+              <Image
+                src="/undraw_chat-interface_vofq.svg"
+                alt="Chat illustration"
+                fill
+                style={{ objectFit: "contain" }}
+                sizes="(max-width: 1200px) 100vw, 400px"
+              />
             </div>
           </div>
         </section>
 
         <section className={styles.sectionContent}>
-          <h2>Section</h2>
           <div className={styles.contentRow}>
             <div>
+              <h2>Section</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo saepe, tempore voluptatibus neque dolores aut quis rem
-                quam corrupti nostrum dolore debitis fugiat vero numquam!
-                Dolores culpa aut voluptatem impedit!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat, mollitia. Ea aspernatur dolorem ipsum aliquam.
+                Inventore dignissimos fugiat laborum blanditiis sed libero, quae
+                a vel necessitatibus, omnis perferendis voluptate voluptas.
               </p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic,
-                repudiandae? Mollitia atque distinctio excepturi nihil officiis
-                explicabo porro voluptates quam praesentium at illo, aliquam ut
-                impedit eos eum sed? Sequi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                velit rem aliquid reprehenderit, nam quod eaque neque mollitia
+                consequuntur. Ea, pariatur voluptas? Natus omnis, ipsam pariatur
+                voluptatem cupiditate quam ex.
               </p>
             </div>
             <div className={styles.imageContainer}>
-              <div className={styles.imagePlaceholder}>
-                <Image
-                  src="/undraw_security-on_3ykb.svg"
-                  alt="Security illustration"
-                  fill
-                  style={{ objectFit: "contain" }}
-                  sizes="(max-width: 1200px) 100vw, 400px"
-                />
-              </div>
+              <Image
+                src="/undraw_security-on_3ykb.svg"
+                alt="Security illustration"
+                fill
+                style={{ objectFit: "contain" }}
+                sizes="(max-width: 1200px) 100vw, 400px"
+              />
             </div>
           </div>
         </section>
